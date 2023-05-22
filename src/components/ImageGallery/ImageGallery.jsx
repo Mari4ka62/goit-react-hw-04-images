083dfcdafbb2ab79b5e7386001c23d5c3c4ff224
Maria Vovchuk <mari4ka62@gmail.com>
2023-05-22 13:59:54 +0300

    .

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -45,67 +45,6 @@ export default function ImageGallery({ value, openModal }) {
     </>
   );
 }
-// export default class ImageGallery extends Component {
-//   state = {
-//     page: 1,
-//     images: [],
-//     isLoading: false,
-//   };
-//   componentDidUpdate(prevProps, prevState) {
-//     if (
-//       prevProps.value !== this.props.value ||
-//       prevState.page !== this.state.page
-//     ) {
-//       this.setState({
-//         isLoading: true,
-//       });
-//       fetchImages(this.props.value, this.state.page)
-// .then(({ hits }) => {
-//   if (hits.length === 0) {
-//     alert('Enter the correct data for the request');
-//   }
-//           this.setState(prevState => ({
-//             images: [...prevState.images, ...hits],
-//           }));
-//         })
-//         .finally(() => {
-//           this.setState({
-//             isLoading: false,
-//           });
-//         });
-//     }
-//   }
-//   nextPage = () => {
-//     this.setState({
-//       page: this.state.page + 1,
-//     });
-//   };
-//   // handleImgUrl = modalImageUrl => {
-//   //   this.setState({ modalImageUrl });
-//   //   this.props.openModal(this.state.modalImageUrl);
-//   // };
-//   render() {
-// return (
-//   <>
-//     <Loader visible={this.state.isLoading} />
-//     <ul className="ImageGallery">
-//       {this.state.images &&
-//         this.state.images.map(({ webformatURL, id, largeImageURL }) => (
-//           <ImageGalleryItem
-//             key={id}
-//             webformatURL={webformatURL}
-//             largeImageURL={largeImageURL}
-//             openModal={this.props.openModal}
-//           />
-//         ))}
-//     </ul>
-//     {this.state.images.length > 0 && !this.state.isLoading && (
-//       <ButtonLoadMore onClick={this.nextPage} />
-//     )}
-//   </>
-// );
-//   }
-// }
 ImageGallery.propTypes = {
   openModal: PropTypes.func,
   images: PropTypes.arrayOf(PropTypes.object),
